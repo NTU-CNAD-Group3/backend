440085c96ab826374b5e53c83681b91741b91c24
YTH-Hank <hank.tc.www.y@gmail.com>
2025-04-04 22:10:09 +0800

    dbv2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# backend
-backend
+# GEN-EXPRESS-CLI
+
+This is a CLI tool to generate express.js boilerplate code.
+
+- [GITHUB](https://github.com/RulerChen/gen-express-cli)
+- [NPM](https://www.npmjs.com/package/gen-express-cli)
---
+++ src/config.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+class Config {
+  constructor() {
+    this.PORT = process.env.PORT || 8000;
+    this.DATABASE_HOST = process.env.DATABASE_HOST || '';
+    this.DATABASE_USER = process.env.DATABASE_USER || '';
+    this.DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || '';
+    this.DATABASE_NAME = process.env.DATABASE_NAME || '';
+    this.DATABASE_PORT = process.env.DATABASE_PORT || '';
+  }
+}
+
+export default new Config();
