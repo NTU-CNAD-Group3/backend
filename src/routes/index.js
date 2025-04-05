65a41f30f40266204bd708ba56efd35efe56f308
YTH-Hank <hank.tc.www.y@gmail.com>
2025-04-05 13:35:54 +0800

    Fabservicev1.3

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,13 @@
 import express from 'express';
 
-<<<<<<< HEAD
-import UserRoutes from '@/routes/user.js';
 import AdminRoutes from '@/routes/admin.js';
 
-=======
->>>>>>> origin/databasev2
 const router = express.Router();
 
 router.get('/healthy', (req, res) => {
   res.send('Auth service is healthy.');
 });
 
-<<<<<<< HEAD
-router.use('/users', UserRoutes);
 router.use('/admin', AdminRoutes);
 
-=======
->>>>>>> origin/databasev2
 export default router;
