3269c75c74b9e07d9a4cdc9d9d4f24b87ef6e1b1
ChunHao-Yang <b10902139@ntu.edu.tw>
2025-04-21 11:21:53 +0800

    fix user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
-import { createController, deleteServerController } from '#src/controllers/user.controller.js';
+import { addServerController, deleteServerController } from '#src/controllers/user.controller.js';
 
 const router = express.Router();
 
-router.post('/', createController); // Create a server
+router.post('/', addServerController); // Create a server
 router.delete('/', deleteServerController); // Delete a server
 
 export default router;
